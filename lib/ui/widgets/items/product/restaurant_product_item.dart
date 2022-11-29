3dbb5111d32d43d46718f6c9ef3a07b37e1219a6
MuzaffarAbduboqiyev <muzaffargenerousboy@gmail.com>
2022-11-29 23:28:23 +0500

    product_detail_screen.dart on going

--- lib/ui/widgets/items/product/restaurant_product_item.dart
+++ lib/ui/widgets/items/product/restaurant_product_item.dart
@@ -1,4 +1,5 @@
 import 'package:delivery_service/model/product_model/product_model.dart';
+import 'package:delivery_service/ui/product/product_detail_screen.dart';
 import 'package:delivery_service/ui/widgets/image_loading/image_loading.dart';
 import 'package:delivery_service/util/theme/theme_methods.dart';
 import 'package:flutter/material.dart';
@@ -18,53 +19,66 @@ class RestaurantProductItem extends StatefulWidget {
 class _RestaurantProductItemState extends State<RestaurantProductItem> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: getCurrentTheme(context).cardColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      margin: const EdgeInsets.all(8),
-      child: Container(
-        padding: const EdgeInsets.all(8),
-        height: 242,
-        width: double.maxFinite,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ImageLoading(
-              imageUrl: widget.productModel.image,
-              imageWidth: double.maxFinite,
-              imageHeight: 145,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            SizedBox(
-              width: double.maxFinite,
-              height: 36,
-              child: Text(
-                widget.productModel.name,
-                style: getCurrentTheme(context).textTheme.bodyLarge,
-                maxLines: 2,
+    return InkWell(
+      onTap: _openProduct,
+      child: Card(
+        color: getCurrentTheme(context).cardColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        margin: const EdgeInsets.all(8),
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          height: 242,
+          width: double.maxFinite,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ImageLoading(
+                imageUrl: widget.productModel.image,
+                imageWidth: double.maxFinite,
+                imageHeight: 145,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              SizedBox(
+                width: double.maxFinite,
+                height: 36,
+                child: Text(
+                  widget.productModel.name,
+                  style: getCurrentTheme(context).textTheme.bodyLarge,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              Text(
+                "\$${widget.productModel.price}",
+                style: getCurrentTheme(context).textTheme.bodyMedium,
+                maxLines: 1,
                 overflow: TextOverflow.ellipsis,
               ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            Expanded(
-              child: Container(),
-            ),
-            Text(
-              "\$${widget.productModel.price}",
-              style: getCurrentTheme(context).textTheme.bodyMedium,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  void _openProduct() async {
+    await showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
+      context: context,
+      builder: (builderContext) =>
+          ProductDetailScreen(productId: widget.productModel.id),
     );
   }
 }
